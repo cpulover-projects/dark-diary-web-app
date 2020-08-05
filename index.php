6f634471711d2172344d44690b71fe4ea08e8416
cpulover <justacpulover@gmail.com>
2020-08-05 17:00:53 +0200

    Connect to database

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Keep your darkness secrets here. I will not tell anyone!";
-?>
+include "properties.php";
+$link = mysqli_connect($host, $username, $password, $database);
+
+if (mysqli_connect_error()) {
+    die("Failed to connect to database<br>");
+} else {
+    echo "Connect to database successfully<br>";
+}
+
