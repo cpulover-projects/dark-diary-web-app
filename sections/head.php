0f87cd21c2ce65754a8061c980d809c40470f32c
cpulover <justacpulover@gmail.com>
2020-08-07 07:27:29 +0200

    REFACTOR: Split page sections

---
+++ sections/head.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="resources/css/style.css">
+
+    <title>The Dark Diary</title>
+
+    <style>
+        html {
+            background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(resources/images/bg.jpg) no-repeat center center fixed;
+        }
+    </style>
+</head>
+
+<body>
