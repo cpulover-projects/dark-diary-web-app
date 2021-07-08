c57f6fcaa164e9f57f16a89e6257d45869774842
unknown <enginoobz@gmail.com>
2021-07-08 20:17:34 +0200

    Update Google Auth redirect URI

--- google-config.php
+++ google-config.php
@@ -13,7 +13,7 @@ $google_client->setClientId('1041610928916-qnns7lkk8ss2dt3h0eu83ampi5pjuh7l.apps
 $google_client->setClientSecret('pt2YlMeO66lchtVzuMAZWd5H');
  
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('http://justacpulover.com/projects/the-dark-diary/main-page.php');
+$google_client->setRedirectUri('https://enginoobz.com/projects/the-dark-diary/main-page.php');
  
 //
 $google_client->addScope('email');
@@ -24,5 +24,3 @@ $google_client->addScope('profile');
 if (!isset($_SESSION)) {
     session_start();
 }
- 
-?>
